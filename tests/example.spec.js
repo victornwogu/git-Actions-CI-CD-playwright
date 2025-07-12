97fc2b304fe83dff1f7fdb06f80f2cf90dccef74
vic <vic@gmail.com>
2025-07-12 14:49:30 +0100

    change

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -6,6 +6,7 @@ test('has title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
+  console.log("Git Actions")
 });
 
 test('get started link', async ({ page }) => {
