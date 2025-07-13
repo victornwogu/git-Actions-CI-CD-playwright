68f9bb8ad3afe38f64f3fa7ce0f8b9d295cf175c
vic <vic@gmail.com>
2025-07-13 15:01:49 +0100

    git reporter

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -6,7 +6,7 @@ test('has title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
-  console.log("Git Actions")
+  console.log("promote")
 });
 
 test('get started link', async ({ page }) => {
